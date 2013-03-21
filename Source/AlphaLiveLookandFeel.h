75f73ccaee6820f3fb58554dbdfa1eed9da22d3d
Liam Lacey <liamlacey@hotmail.com>
2013-03-21 11:23:31 +0000

    Added licensing information to the headers of each file.

--- Source/AlphaLiveLookandFeel.h
+++ Source/AlphaLiveLookandFeel.h
@@ -1,11 +1,24 @@
-/*
- *  AlphaLiveLookandFeel.h
- *  AlphaLive
- *
- *  Created by Sam Davies on 12/06/2012.
- *  Copyright 2012 nu desine. All rights reserved.
- *
- */
+//
+//  AlphaLiveLookandFeel.h
+//  AlphaLive Updater
+//
+//  Created by Liam Meredith-Lacey on 18/03/2013.
+//  Copyright 2013 nu desine.
+//
+//  This file is part of AlphaLive Updater.
+//
+//  AlphaLive Updater is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License, version 2, 
+//  as published by the Free Software Foundation.
+//  
+//  AlphaLive Updater is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
 
 
 #ifndef ALPHALIVELOOKANDFEEL_H
--- Source/MainComponent.h
+++ Source/MainComponent.h
@@ -6,6 +6,28 @@
   ==============================================================================
 */
 
+//
+//  MainComponent.h
+//  AlphaLive Updater
+//
+//  Created by Liam Meredith-Lacey on 18/03/2013.
+//  Copyright 2013 nu desine.
+//
+//  This file is part of AlphaLive Updater.
+//
+//  AlphaLive Updater is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License, version 2, 
+//  as published by the Free Software Foundation.
+//  
+//  AlphaLive Updater is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
 #ifndef __MAINCOMPONENT_H_ED384941__
 #define __MAINCOMPONENT_H_ED384941__
 
