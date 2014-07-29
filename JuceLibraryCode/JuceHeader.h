6526bab1c7bcc3265bac9fa0384384de36583bef
Liam Lacey <liamlacey@hotmail.com>
2014-07-29 10:53:16 +0100

    Upped the version number to 1.0.4 for OS X.

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -37,8 +37,8 @@
 namespace ProjectInfo
 {
     const char* const  projectName    = "AlphaLive Updater";
-    const char* const  versionString  = "1.0.3";
-    const int          versionNumber  = 0x10003;
+    const char* const  versionString  = "1.0.4";
+    const int          versionNumber  = 0x10004;
 }
 
 #endif   // __APPHEADERFILE_FTMW3Q__
